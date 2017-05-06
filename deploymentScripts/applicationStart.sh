5daab6891d3422323f8a2cd0ea5c15f704751c26
amri <a.amri.91@gmail.com>
2017-05-06 03:26:40 +0300

    changing scripts

--- deploymentScripts/applicationStart.sh
+++ deploymentScripts/applicationStart.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
-pm2-docker startOrReload /webapps/myApp/ecosystem.config.js
-pm2-docker flush
+#set -euo pipefail
+set -eu pipefail
+node /webapps/myApp/index.js
+#pm2-docker startOrReload /webapps/myApp/ecosystem.config.js
+#pm2-docker flush
