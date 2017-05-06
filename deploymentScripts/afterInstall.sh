d7f709ef512e9dc2c6b48fc8d33d012522167c42
amri <a.amri.91@gmail.com>
2017-05-06 04:29:19 +0300

    debugging scripts

--- deploymentScripts/afterInstall.sh
+++ deploymentScripts/afterInstall.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo touch afterInstallTrace.txt
 set -euo pipefail
 cd /webapps/myApp
 npm install
--- deploymentScripts/applicationStart.sh
+++ deploymentScripts/applicationStart.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #set -euo pipefail
+sudo touch appStartTrace.txt
 set -eu pipefail
 node /webapps/myApp/index.js
 #pm2-docker startOrReload /webapps/myApp/ecosystem.config.js
